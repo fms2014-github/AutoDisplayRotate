5aceeb0bf4864e7e6f279a36b0963c8114909c75
DESKTOP-DCDI967\fms20 <fms2014@naver.com>
2022-03-27 01:29:43 +0900

    -ArduinoComuication 클래스에 Serial 장치 목록 메소드 추가 -ArduinoComuication 클래스에 장치 연결 메소드 추가 및 오버로드 작성

--- Core/ArduinoComuication.cs
+++ Core/ArduinoComuication.cs
@@ -2,11 +2,65 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO.Ports;
 
-namespace AutoDisplayRotate.MVVM.ViewModel
+/*
+ * 
+ * 코드 참고 1 : https://unininu.tistory.com/304
+ * 코드 참고 2 : https://diy-dev-design.tistory.com/129
+ * 
+ */
+
+namespace AutoDisplayRotate.Core
 {
     internal class ArduinoComuication
     {
+        private SerialPort serialPort { get; }
+
+        public ArduinoComuication()
+        {
+            serialPort = new SerialPort();
+        }
+   
+        public string[] connectableDeviceList()
+        {
+            return SerialPort.GetPortNames();
+        }
+
+        public void deviceConnect(string portName, SerialDataReceivedEventHandler e)
+        {
+            serialPort.PortName = portName;
+            serialPort.BaudRate = 9600;
+            serialPort.DataBits = 8;
+            serialPort.StopBits = StopBits.One;
+            serialPort.Parity = Parity.None;
+            serialPort.DataReceived += e;
+
+            serialPort.Open();  //시리얼포트 열기
+        }
+
+        public void deviceConnect(string portName, int baudRate, SerialDataReceivedEventHandler e)
+        {
+            serialPort.PortName = portName;
+            serialPort.BaudRate = baudRate;
+            serialPort.DataBits = 8;
+            serialPort.StopBits = StopBits.One;
+            serialPort.Parity = Parity.None;
+            serialPort.DataReceived += e;
+
+            serialPort.Open();  //시리얼포트 열기
+        }
+
+        public void deviceConnect(string portName, int baudRate, int dataBits, SerialDataReceivedEventHandler e)
+        {
+            serialPort.PortName = portName;
+            serialPort.BaudRate = baudRate;
+            serialPort.DataBits = dataBits;
+            serialPort.StopBits = StopBits.One;
+            serialPort.Parity = Parity.None;
+            serialPort.DataReceived += e;
+
+            serialPort.Open();  //시리얼포트 열기
+        }
     }
 }
